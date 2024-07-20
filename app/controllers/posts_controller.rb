f76e34ba1c62eef312422c406e5ece66db38a1f6
AmiasYaska <amiasyaska@gmail.com>
2024-07-21 00:51:12 +0300

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  include ActiveStorage::SetCurrent
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index]
 
