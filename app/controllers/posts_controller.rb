5bc9f86f87b367017bb2df37deb142b28ae8659c
AmiasYaska <amiasyaska@gmail.com>
2024-07-21 12:28:45 +0300

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,6 +67,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:video, :description)
+      params.require(:post).permit(:video, :description, :title)
     end
 end
