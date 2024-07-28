fdedfa2e494048396f48216978f4aaec60dbb276
AmiasYaska <amiasyaska@gmail.com>
2024-07-28 12:28:11 +0300

    changes

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -3,6 +3,7 @@ module Posts
         def create
             @post = Post.find(params[:post_id])
             current_user.likes.create(post_id: @post_id)
+            turbo_stream: turbo_stream.update(dom_id(@post, :like)), partial: "posts/like_button", locals: {post, like}
         end
     end
 end
