74969e6ea975887db1143859b2531b95c8407c48
AmiasYaska <amiasyaska@gmail.com>
2024-07-28 13:03:23 +0300

    changes

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,9 +1,11 @@
 module Posts
     class LikesController < ApplicationController
+        include ActionView::RecordIdentifier
         def create
             @post = Post.find(params[:post_id])
-            current_user.likes.create(post_id: @post_id)
-            turbo_stream: turbo_stream.update(dom_id(@post, :like)), partial: "posts/like_button", locals: {user: current_user, post: @post}
+            current_user.likes.create(post_id: @post.id)
+            render turbo_stream: turbo_stream.update(dom_id(@post, :like), partial: "posts/like_button", 
+            locals: {user: current_user, post: @post})
         end
     end
 end
